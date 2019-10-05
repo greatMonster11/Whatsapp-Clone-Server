9bcefadf5b2d8e4a0f82d04f4b00fbbde522bb4b
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-10-06 00:09:33 +0700

    finish setup server

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
+import { chats } from './db';
 
 const app = express();
+app.use(cors());
 
-app.get("/_ping", (req, res) => {
-  res.send("pong");
+app.get('/_ping', (req, res) => {
+  res.send('pong');
+});
+
+app.get('/chats', (req, res) => {
+  res.json(chats);
 });
 
 const port = process.env.PORT || 4000;
